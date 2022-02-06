577078b54d8afa20aaa440d28466cf4fa97e4430
raocz <1963196695@qq.com>
2022-02-06 16:35:45 +0800

    chore: fix config

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -23,4 +23,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:cz1997-start/blog.git main:gh-pages
+git push -f git@github.com:cz1997-start/blog.git master:gh-pages
